d7897dd6d2dc678a56d3efe58b3d4261b6550845
TechOgede <sammonius@live.com>
2023-03-15 06:48:31 -0700

    basic calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def main():
+    import calculator_1 as calc
+    import sys
+    operators = ["+", "-", "*", "/"]
+    if len(sys.argv) != 4:
+        print("Usage ./100-my_calculator.py <a> operator <b>")
+        exit(1)
+    if sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+        print("{} + {} = {}".format(a, b, calc.add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{} - {} = {}".format(a, b, calc.sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{} * {} = {}".format(a, b, calc.mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{} / {} = {}".format(a, b, calc.div(a, b)))
+
+
+if __name__ == "__main__":
+    main()
