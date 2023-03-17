18a2f2e395f695ccade06db4bd586e6e49dd0934
TechOgede <sammonius@live.com>
2023-03-17 04:42:49 -0700

    return dict key with max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
     max_val = max(a_dictionary.values())
     for k, v in a_dictionary.items():
