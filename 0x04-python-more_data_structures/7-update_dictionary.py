d71c7af59902cbb82ff082db8121985671f8437e
TechOgede <sammonius@live.com>
2023-03-17 03:48:47 -0700

    Update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    new_dict = {}
-    for k, v in a_dictionary.items():
-        new_dict[k] = v
-    new_dict[key] = value
-    return (new_dict)
+    a_dictionary[key] = value
+    return (a_dictionary)
