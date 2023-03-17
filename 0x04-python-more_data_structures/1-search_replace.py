f729c22854316bdcf564450c40f45238ce586979
TechOgede <sammonius@live.com>
2023-03-17 02:38:47 -0700

    search and replace element in list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,5 @@
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
     index = new_list.index(search)
-    new_list.insert(index, replace)
-    new_list.remove(search)
+    new_list[index] = replace
     return (new_list)
