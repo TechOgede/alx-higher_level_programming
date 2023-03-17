4d78200c970b92c5df7e0e0e3961852b28c06f1e
TechOgede <sammonius@live.com>
2023-03-17 09:15:54 -0700

    roman numerals to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
+    if not isinstance(roman_string, str):
+        return 0
     num = 0
     i = 0
     r_nums = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     roman = roman_string.upper()
-    if len(roman) == 1:
-        return r_nums[roman]
     while i < len(roman):
         curr = roman[i]
         if (i + 1 < len(roman)):
