7bf43d97d2aacaa48ed8f77a47305867a9ec08a0
TechOgede <sammonius@live.com>
2023-03-17 01:34:07 -0700

    square elements of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,3 @@ def square_matrix_simple(matrix=[]):
     sqrd_matrix = [[matrix[i][j] ** 2 for j in range(len(matrix[i]))]
                    for i in range(len(matrix))]
     return (sqrd_matrix)
-
