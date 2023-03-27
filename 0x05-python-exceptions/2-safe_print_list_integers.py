79dcfd418cf247b0ea4058a534fdb33aef0565e9
TechOgede <sammonius@live.com>
2023-03-27 10:02:33 -0700

    catching and handling ValueError and TypeError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,8 +4,6 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except IndexError:
-            break
         except ValueError:
             continue
         except TypeError:
