cce20179e8395ef8283e3c6d4da2a71aa9021189
TechOgede <sammonius@live.com>
2023-03-27 08:50:25 -0700

    catching and handling out of range error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x <= 0:
-        return 0
-    try:
-        my_list[x - 1]
-    except:
-        i = 0
-        for x in my_list:
-            print("{}".format(x), end='')
-            i += 1
-        print()
-        return i
-    else:
-        for i in range(x):
+    cnt = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end='')
-        print()
-        return x
-
+        except:
+            break
+        else:
+            cnt += 1
+    print()
+    return cnt
