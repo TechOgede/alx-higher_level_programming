a759fdccfeec74e8ede4856bc436da7e170871c4
TechOgede <sammonius@live.com>
2023-03-29 09:17:57 -0700

    added another priv var, position

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+''' This Module defines a Square class. It has one private instance
+    variable, size.'''
+
+
+class Square:
+    ''' This class has one private instance variable'''
+    def __init__(self, size=0, position=(0, 0)):
+        '''Args:
+            size (int): positive integer. Denotes the size of a square.
+            position (tuple): consists of 2 positive integers. Denotes coordinates
+        '''
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__position = position
+        if not isinstance(self.__position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            if not isinstance(position[0], int) or\
+             not isinstance(position[1], int):
+                raise TypeError("position must be a tuple\
+                                of 2 positive integers")
+            if len(position) != 2 or position[1] < 0 or position[0] < 0:
+                raise TypeError("position must be a tuple\
+                                of 2 positive integers")
+
+    @property
+    def size(self):
+        ''' Getter for the size
+        Returns:
+          an integer. the size variable'''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' Setter for the priv var, size
+        Args:
+          value: value to which size is updated'''
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        ''' Retrieves the position coordinates
+        Returns:
+          a tuple bearing the coordinates '''
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        '''Setter for priv var, position
+        Args:
+          value: value to which position is set'''
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[1], int) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive intergers")
+        self.__position = value
+
+    def area(self):
+        ''' Calculates the area of the square
+            Returns:
+              an integer
+        '''
+        return self.__size * self.__size
+
+    def my_print(self):
+        ''' Prints the square using '#' '''
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end='')
+                for k in range(self.__size):
+                    print('#', end='')
+                print()
