a529d57f625732cf9a3d6a86c4763288724343cb
TechOgede <sammonius@live.com>
2023-03-28 17:36:30 -0700

    created a private instance variable for Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' This Module defines a Square class. It has one private instance
+    variable, size.'''
+
+
+class Square:
+    ''' This class has one private instance variable'''
+    def __init__(self, size):
+        '''Args:
+            size: denotes the size of a square.
+        '''
+        self.__size = size
