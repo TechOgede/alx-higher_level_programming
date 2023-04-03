adce0c49877ea2111c913a0f0463ad7d40db92d1
TechOgede <sammonius@live.com>
2023-04-03 05:32:07 -0700

    Defines an empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' This module defines an empty class '''
+
+
+class Rectangle:
+    ''' Rectangle is an empty class '''
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,5 @@
+Going deeper into Classes in Python
+-Class and instance attributes
+-classmethods and staticmethods
+-properties
+-Getters and setters
