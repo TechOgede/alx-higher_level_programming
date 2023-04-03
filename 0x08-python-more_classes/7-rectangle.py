f1f5ed8f34d6b346f16455daaf61154fc75688c8
TechOgede <sammonius@live.com>
2023-04-03 11:49:37 -0700

    added a class attr, print_symbol

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        ''' Calculates the perimeter of the rectangle '''
+        ''' Determines the perimeter of the rectangle '''
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
