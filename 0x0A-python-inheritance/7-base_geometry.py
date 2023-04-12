884da77736f5103cfc2c2b5b701388d33ce4e593
TechOgede <sammonius@live.com>
2023-04-12 05:47:51 -0700

    fixed bug in 7-base_geometry: integer validator now checks for exact instance of int

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,7 +9,8 @@ class BaseGeometry:
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
+        ''' Methods that checks if a value is an int or > 0 '''
+        if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,25 +3,14 @@
     Rectangle inherits the class, BaseGeometry'''
 
 
-class BaseGeometry:
-    ''' Has two public instance methods, area() and integer_validator'''
-    def area(self):
-        ''' Raises an exception '''
-        raise Exception('area() is not implemented')
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int) or not type(value) is int:
-            raise TypeError('{} must be an integer'.format(name))
-        if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     ''' Has two atrributes, width and height.
         They represent the sides of a rectange '''
 
     def __init__(self, width, height):
-        BaseGeometry.integer_validator(self, "width", width)
-        BaseGeometry.integer_validator(self, "height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
