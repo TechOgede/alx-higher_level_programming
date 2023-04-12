e1d7ee639fdd6bc0049062b72cc2371b41c07feb
TechOgede <sammonius@live.com>
2023-04-12 05:49:10 -0700

    fixed bug in 7-base_geometry: integer validator now checks for exact instance of int

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,6 +5,7 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     ''' Has two atrributes, width and height.
         They represent the sides of a rectange '''
