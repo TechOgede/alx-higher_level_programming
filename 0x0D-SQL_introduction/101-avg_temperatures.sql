6fe8dde4c242658a6353210ae802f6bce050a033
TechOgede <sammonius@live.com>
2023-05-17 16:16:54 -0700

    display query results from temperatures table

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,6 @@
+-- Displays average temperature by city
+
+-- Displays average temperature by city from the table dump
+SELECT city, AVG(value) AS avg_temp FROM temperatures
+GROUP BY city
+ORDER BY value DESC;
