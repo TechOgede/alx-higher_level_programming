f9efa5be1cae4a4f5aaa07db1b684b9a6649d63b
TechOgede <sammonius@live.com>
2023-05-17 10:48:16 -0700

    sql queries

---
+++ 0x0D-SQL_Introduction/0-list_databases.sql
@@ -0,0 +1,4 @@
+-- Shows all the databases in the server
+
+-- Lists all dataases in the server
+SHOW DATABASES;
---
+++ 0x0D-SQL_Introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- Lists rows in a table
+
+-- Lists rows in table, first_table
+SELECT * FROM first_table;
