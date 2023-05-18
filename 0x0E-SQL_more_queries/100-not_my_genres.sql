b8f4e3e9de2941a723ff026de111924dfa918e44
TechOgede <sammonius@live.com>
2023-05-18 15:59:33 -0700

    lists all genres not linked to a show

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,16 @@
+-- Lists shows in a database
+-- Lists all genres not linked to the show, 'Dexter',w in hbtn_0d_tvshows
+WITH dexter_genres AS (
+        SELECT tv_genres.id, tv_genres.name
+        FROM tv_genres JOIN tv_show_genres
+            ON tv_genres.id = tv_show_genres.genre_id
+            JOIN tv_shows
+            ON tv_show_genres.show_id = tv_shows.id
+        WHERE tv_shows.title = 'Dexter')
+
+SELECT tv_genres.name
+FROM tv_genres
+LEFT JOIN dexter_genres
+ON tv_genres.id = dexter_genres.id
+WHERE dexter_genres.id IS NULL
+ORDER BY tv_genres.name;
