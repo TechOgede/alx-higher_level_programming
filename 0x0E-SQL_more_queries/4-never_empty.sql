476b5c9023942e0109a8db8a9a3b8c24222152c6
TechOgede <sammonius@live.com>
2023-05-18 12:16:13 -0700

    creates table "id_not_null"

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
 -- Creates a table
 -- Creates the table, id_not_null
 
-CREATE TABLE IF NOT EXISTS force_name (id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
