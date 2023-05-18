d114986ebadf6ce27094f88cf318ae595ae55d3f
TechOgede <sammonius@live.com>
2023-05-18 13:27:19 -0700

    lists all cities contained in hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all the cities contained in the database
+-- Lists all the cities contained in hbtn_0d_usa
+
+SELECT cities.id, cities.name, states.name
+FROM cities JOIN states
+     ON cities.state_id = states.id
+ORDER BY cities.id;
