9c8e8ee9c0e8a4391aad1f90e636a45ff02e1083
TechOgede <sammonius@live.com>
2023-06-15 07:52:38 -0700

    factored in edge case

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,11 @@
 let nums = process.argv.slice(2);
 nums = nums.map((num) => Number(num));
 
-let max = Math.max.apply(null, nums);
-const idx = nums.indexOf(max);
-nums.splice(idx, 1);
-max = Math.max.apply(null, nums);
-
 if (nums.length > 1) {
+  let max = Math.max.apply(null, nums);
+  const idx = nums.indexOf(max);
+  nums.splice(idx, 1);
+  max = Math.max.apply(null, nums);
   console.log(max);
 } else {
   console.log(0);
