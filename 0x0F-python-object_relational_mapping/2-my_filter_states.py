accb2944afa4ba99b76ade249e1697084055e842
TechOgede <sammonius@live.com>
2023-06-17 04:48:12 -0700

    displays values in states that match a state name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+''' This script lists all states with a name starting with N
+    from the database hbtn_0c_0_usa '''
+
+
+def main():
+    ''' lists all states from a database using MySQLdb ORM '''
+    import MySQLdb
+    import sys
+
+    user_name = sys.argv[1]
+    pass_word = sys.argv[2]
+    data_base = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=user_name,
+                         passwd=pass_word, db=data_base, charset='utf8')
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id", (state,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
