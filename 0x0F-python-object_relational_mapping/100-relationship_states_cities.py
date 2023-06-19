7be66f3aa1923c5789928b567e77ac76f3f5c897
TechOgede <sammonius@live.com>
2023-06-19 05:20:10 -0700

    established a one-to-many relationship btw State and City using backref

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -2,25 +2,30 @@
 ''' This script creates the State "CAlifornia" with the
 City "San Francisco" from the database hbtn_0e_100_usa '''
 
+def main():
+    ''' Driver funcion '''
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from relationship_state import Base, State
+    from relationship_city import City
 
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from relationship_city import City
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                       format(sys.argv[1], sys.argv[2],
-                              sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    california = State(name='California')
+    san_fran = City(name='San Francisco', state=california)
 
-california = State(name='California')
-san_fran = City(name='San Francisco', state=california)
+    session.add_all([california, san_fran])
 
-session.add_all([california, san_fran])
+    session.commit()
 
-session.commit()
+
+if __name__ == '__main__':
+    main()
