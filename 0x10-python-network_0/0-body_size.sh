cc960ec529aa7873518b0086309c619c9fcd8c3d
TechOgede <sammonius@live.com>
2023-06-29 08:19:55 -0700

    fixed hashbang line

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-#Takes a URL, eends a request to it and displays the size of the body of the response
+#!/bin/bash
+#Takes a URL, sends a request to it and displays the size of the body of the response
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f 2
