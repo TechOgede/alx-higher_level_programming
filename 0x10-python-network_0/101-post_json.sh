b64b2a1311c4c633d7bcef205d9567eb361eae21
TechOgede <sammonius@live.com>
2023-06-29 20:05:50 +0100

    send JSON POST request

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #Sends a POST request to a URL using data stored in a file
-curl -s -X "POST" -H "Content-Type: application/json" -d @"$2" "$1"
+curl -s -H "Content-Type: application/json" -d @"$2" "$1"
 
