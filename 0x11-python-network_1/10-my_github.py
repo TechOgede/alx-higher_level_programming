600a40077e68ee6f67d9223e21ef46a6bf0effef
TechOgede <sammonius@live.com>
2023-06-30 19:46:08 +0100

    queries GitHub api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' This script takes GitHub credentials and uses the GitHub
+API to display id
+'''
+
+
+def main():
+    ''' Driver function '''
+    import sys
+    import requests
+
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+
+    url = f'https://api.github.com/{user}'
+    headers = {'Accept': 'application/vnc.github+json',
+               'Authorization': f'Bearer {pwd}',
+               'X-GitHub-Api-Version': '2022-11-28'}
+    res = requests.get(url, headers=headers)
+    data = res.json()
+    print(data.get('id'))
+
+
+if __name__ == '__main__':
+    main()
