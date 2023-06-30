2932aca4f54a987ef7f3333aea9668d1cd73d84c
TechOgede <sammonius@live.com>
2023-06-30 16:01:57 +0100

    sneds a POST req with a param

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,7 +10,9 @@ def main():
     import urllib.request
 
     url = sys.argv[1]
-    data = {email: sys.argv[2]}
+    value = {email: sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
         page = response.read()
