ab7170ba0005be54405ace5cba2010fd4401e0c9
TechOgede <sammonius@live.com>
2023-06-30 15:56:45 +0100

    sneds a POST req with a param

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+''' This scirpt takes a URL and an email,
+    sends a POST request to the URL with the email as a parameter,
+    and displays the value of the 'X-Request-Id' variable'''
+
+
+def main():
+    ''' Driver function '''
+    import sys
+    import urllib.request
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    req = urllib.request.Request(url, email)
+    with urllib.request.urlopen(req) as response:
+        page = response.read()
+
+    print(page.decode('utf-8')
+
+
+if __name__ == '__main__':
+    main()
