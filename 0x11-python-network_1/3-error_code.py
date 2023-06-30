e67727f2a386a538cd90d3fe24df46d488fa6523
TechOgede <sammonius@live.com>
2023-06-30 16:39:56 +0100

    manages HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+''' This scirpt takes a URL, sends a request to the URL and
+displays the value of the 'X-Request-Id' variable or 
+the error code, if any'''
+
+
+def main():
+    ''' Driver function '''
+    import sys
+    import urllib.request
+    import urllib.error
+
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f'Error code: {e.code}')
+    
+
+
+if __name__ == '__main__':
+    main()
