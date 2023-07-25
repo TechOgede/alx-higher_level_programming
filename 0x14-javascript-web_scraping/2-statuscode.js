ed14884d7f04a85629f33b70a9272aef3092240f
TechOgede <sammonius@live.com>
2023-07-25 11:51:57 -0700

    displays status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,6 +6,6 @@ request.get(url, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    console.log(response && response.statusCode);
+    console.log(response && `code: ${response.statusCode}`);
   }
 });
