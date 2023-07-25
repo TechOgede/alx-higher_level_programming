d8910fe6fa3b58dc1a95dc98c6657e346f1d298f
TechOgede <sammonius@live.com>
2023-07-25 13:46:41 -0700

    computes number of completed tasks by user id

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const tasks = JSON.parse(body);
+
+    const usersAndTasks = {};
+
+    for (const task of tasks) {
+      if (task.completed) {
+        if (task.userId in usersAndTasks) {
+          usersAndTasks[`${task.userId}`]++;
+        } else {
+          usersAndTasks[`${task.userId}`] = 1;
+        }
+      }
+    }
+    console.log(usersAndTasks);
+  }
+});
